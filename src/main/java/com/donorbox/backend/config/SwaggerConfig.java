bd0596fafd6aa415b58766ab40130d6ce612710c
Abhishek Mahajan <abhishekmahajan@alphaseam.com>
2025-07-23 13:14:40 +0530

    minor swagger documentation changes

--- src/main/java/com/donorbox/backend/config/SwaggerConfig.java
+++ src/main/java/com/donorbox/backend/config/SwaggerConfig.java
@@ -19,20 +19,12 @@ public class SwaggerConfig {
         localServer.setUrl("http://localhost:8080");
         localServer.setDescription("Server URL for Local Development");
 
-        Contact contact = new Contact();
-        contact.setEmail("support@donorbox.com");
-        contact.setName("Donorbox Support");
-
-        License license = new License()
-                .name("MIT License")
-                .url("https://choosealicense.com/licenses/mit/");
 
         Info info = new Info()
                 .title("Donorbox Crowdfunding Platform API")
                 .version("1.0")
-                .contact(contact)
                 .description("API documentation for the Donorbox crowdfunding platform backend with international payment support")
-                .license(license);
+              ;
 
         return new OpenAPI()
                 .info(info)
