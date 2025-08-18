bdc32740b8c378890b9efa9d8d67d2adf858aa8d
Abhishek Mahajan <abhishekmahajan@alphaseam.com>
2025-08-18 15:43:27 +0530

    email donation fix

--- src/main/java/com/donorbox/backend/repository/DonationRepository.java
+++ src/main/java/com/donorbox/backend/repository/DonationRepository.java
@@ -3,13 +3,10 @@ package com.donorbox.backend.repository;
 import com.donorbox.backend.entity.Donation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DonationRepository extends JpaRepository<Donation, Long> {
     Optional<Donation> findByOrderId(String orderId);
     Optional<Donation> findByPaymentId(String paymentId);
-    List<Donation> findByStatus(Donation.DonationStatus status);
 }
